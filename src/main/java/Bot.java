ca9df96a51395f2993dea32915f9e554fbc5f60e
Marlon Aquino <marlon.aquino24@gmail.com>
2019-05-20 09:35:09 -0700

    Added token and bot

---
+++ src/main/java/Bot.java
@@ -0,0 +1,11 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Bot {
+
+    public static void main(String[] args) throws Exception{
+
+        JDA jda = new JDABuilder("NTgwMDY4NTU5NDk4MTE3MTQz.XOLVfQ.Vx3E_IMpv_jO_rNcw-QQ40AOjeE").build();
+
+    }
+}
