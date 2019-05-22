274983ce2636824299b9567666f72fb6b4d6d37e
Marlon Aquino <marlon.aquino24@gmail.com>
2019-05-21 20:39:23 -0700

    Bug fixes
    Fixed an issue where the bot was calling the !snap command if it encountered it previousl causing an infinite loop

--- src/main/java/events/ThanosEvent.java
+++ src/main/java/events/ThanosEvent.java
@@ -13,42 +13,31 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class ThanosEvent extends ListenerAdapter {
 
     //Listen for !thanos message being sent on Discord server
-    public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         //Collect the message and store it
         //Stores the message of that event into the String messageSent. Returns the string version of that message.
         //event object comes with different methods
         //Put each item into an array, whenever there is a space, put it into an array
         String messageSent = event.getMessage().getContentRaw();
 
+
         //Clears half of the messages in discord chat
-        if (messageSent.equalsIgnoreCase("!snap")){
-            //Count how many messages are sent in channel
-            int messageCount = 0;
-            Random rand = new Random();
-            List<Message> messages = event.getChannel().getHistory().retrievePast(100).complete();
-            for (int i = 0; i < messages.size(); i++){
-                messageCount = messageCount + 1;
+        if (messageSent.equalsIgnoreCase("!snap")) {
+            List<Message> messages;
+            Random random = new Random();
+
+            //Get most recent 100 channel messages
+            //There is a limit where you can only delete a max of 100 messages
+            //TODO: Find a way to bypass that 100 message limit
+
+            messages = event.getChannel().getHistory().retrievePast(100).complete();
+            for ( int i = 0; i < (messages.size() / 2); i++){
+                int randomIndex = random.nextInt(messages.size());
+                messages.remove(randomIndex);
             }
-
-            int halfMessage = messageCount / 2;
-
-            List<Message> toDelete = new ArrayList<Message>();
-
-            for (int j = 0; j < halfMessage; j++){
-                int randomIndex = rand.nextInt(messages.size());
-                Message randomElement = messages.get(randomIndex);
-                toDelete.add(randomElement);
-            }
-
-            //Sends the message in the same channel that the message was displayed in
-            //Get the channel that the event happened in.
-            //Want to add .queue at the end bc bots have lots of things to do at once.
-            event.getChannel().sendMessage("I am inevitable.").queue();
-            event.getChannel().deleteMessages(toDelete).queue();
-
+            event.getChannel().deleteMessages(messages).queue();
 
         }
     }
-
-    }
+}
 
